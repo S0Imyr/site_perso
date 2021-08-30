38113de5b0bcce2dfe27527b7a4b8d087cbcb87e
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-08-30 15:57:29 +0200

    Delete assets refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import  { Redirect, Route, Switch } from 'react-router-dom';
+import Home from './components/Home'
+import Contact from './components/Contact';
+import { Project1, Project2 } from './components/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/project-1" component={Project1} />
+      <Route exact path="/project-2" component={Project2} />
+      <Route exact path="/contact" component={Contact} />
+      <Redirect to="/" />
+    </Switch>
   );
 }
 
