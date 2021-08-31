1eb39dc5f2e642e789eebccd9ed06fe4a4006d84
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-08-31 17:01:11 +0200

    Expériences component

--- src/components/skills/Experience.js
+++ src/components/skills/Experience.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 const Experience = () => {
     return (
-        <div>
-            Expériences
+        <div className="experience">
+            <h3>Expériences</h3>
+            <div className="exp1">
+                <h4>Fonction1</h4>
+                <h5>Dates-Dates</h5>
+                <p>
+                    Contenu
+                </p>
+            </div>
+            <div className="exp2">
+                <h4>Fonction2</h4>
+                <h5>Dates-Dates</h5>
+                <p>
+                    Contenu
+                </p>
+            </div>
         </div>
     );
 };
