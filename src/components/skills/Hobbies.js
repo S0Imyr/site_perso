d67ef786cd1b13bacd23e6ba5abb0de1c03dabf3
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-09-01 01:32:31 +0200

    Add Hobbies

--- src/components/skills/Hobbies.js
+++ src/components/skills/Hobbies.js
@@ -2,8 +2,26 @@ import React from 'react';
 
 const Hobbies = () => {
     return (
-        <div>
-            Passions
+        <div className="hobbies">
+            <h3>Intérêts</h3>
+            <ul>
+                <li className="hobby">
+                    <i className="fas fa-landmark"></i>
+                    <span>Histoire antique</span>
+                </li>
+                <li className="hobby">
+                    <i className="fas fa-bicycle"></i>
+                    <span>Vélo</span>
+                </li>
+                <li className="hobby">
+                    <i className="fas fa-satellite-dish"></i>
+                    <span>Espace</span>
+                </li>
+                <li className="hobby">
+                    <i className="fas fa-atom"></i>
+                    <span>Histoire des Sciences</span>
+                </li>
+            </ul>
         </div>
     );
 };
