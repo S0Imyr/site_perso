d3907d4046c9ec814d7876422b998c04454baead
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-08-31 15:34:07 +0200

    Add Home paragraph

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,9 @@ function Home() {
         <Navigation/>
         <div className="homeContent">
           <div className="content">
-            <h1>Raphaël Lossec | Développeur Python</h1>
-            <h2>Bonjour et bienvenue !</h2>
+            <h1>Raphaël Lossec</h1>
+            <h2>Développeur Python</h2>
+            <p>Bonjour et bienvenue !</p>
             <div className="pdf">
               <a href="./media/CV.pdf" target="_blank">Télécharger CV</a>
             </div>
