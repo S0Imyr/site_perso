c3dc1f91cd93edb8a02f67117f6d79cd8ecc389b
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-09-01 01:33:03 +0200

    Add Languages

--- src/components/skills/Languages.js
+++ src/components/skills/Languages.js
@@ -4,25 +4,19 @@ import ProgressBar from './ProgressBar';
 class Languages extends Component {
     state = {
         languages: [
-            {id: 1, value: "Python", xp:8.2},
-            {id: 2, value: "Javascript", xp:5.4},
-            {id: 3, value: "VBA", xp:4.4},
-            {id: 4, value: "R", xp:3.5},
+            {id: 1, value: "Python", xp:8.8},
+            {id: 2, value: "Javascript", xp:6},
+            {id: 3, value: "VBA", xp:4},
+            {id: 4, value: "R", xp:3},
         ],
         frameworks: [
-            {id: 1, value: "Django", xp:7.5},
-            {id: 2, value: "Sass", xp:6},
+            {id: 1, value: "Django", xp:8.5},
+            {id: 2, value: "Sass", xp:7},
             {id: 3, value: "React", xp:5.4},
-        ],
-        tools: [
-            {id: 1, value: "Git", xp:8},
-            {id: 2, value: "Github", xp:7},
-            {id: 3, value: "VSCode", xp:6},
-            {id: 4, value: "Docker", xp:3},
         ]
     }
     render() {
-        let {languages, frameworks, tools} = this.state;
+        let {languages, frameworks} = this.state;
 
         return (
             <div className="knowledges">
@@ -35,11 +29,6 @@ class Languages extends Component {
                     data={frameworks}
                     className="frameworksDisplay"
                     title="frameworks"
-                />
-                <ProgressBar 
-                    data={tools}
-                    className="toolsDisplay"
-                    title="tools"
                 />
             </div>
         );
