3a4821bc9c76b16a3eb484dd0f75ba77355e7943
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-08-31 14:18:45 +0200

    Home page style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,17 @@ import Navigation from '../components/Navigation';
 
 function Home() {
     return (
-      <div>
+      <div className="home">
         <Navigation/>
+        <div className="homeContent">
+          <div className="content">
+            <h1>Raphaël Lossec | Développeur Python</h1>
+            <h2>Bonjour et bienvenue !</h2>
+            <div className="pdf">
+              <a href="./media/CV.pdf" target="_blank">Télécharger CV</a>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
