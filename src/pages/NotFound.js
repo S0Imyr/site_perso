6468a2f661a1f183eafb46a1628c124862154fd0
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-08-31 15:16:47 +0200

    Not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            Not found !
+        <div className="notFound">
+            <div className="notFoundContent">
+                <img src="./media/broken.png" alt="Not found"/>
+                <h3>Désolé, il n'y a pas de page à cet url.</h3>
+                <NavLink exact to="/">
+                    <i className="fas fa-home"></i>
+                    <span> Accueil</span>
+                </NavLink>
+            </div>
         </div>
     );
 };
