2b94c3f0572a9738888c7c6c0a09c66c2b664a25
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-09-01 16:02:14 +0200

    Add pop up description for Project

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 
 class Project extends Component {
+    state = {
+        showInfo: false
+    }
+
+    handleInfo = () => {
+        this.setState({
+            showInfo:!this.state.showInfo
+        })
+    }
+
+
     render() {
-        let {title, languagesIcons, source, info, img} = this.props.item;
+        let {title, languagesIcons, link, resume, img} = this.props.item;
         return (
             <div className='project'>
                 <div className='icons'>
@@ -11,10 +22,31 @@ class Project extends Component {
                     )}
                 </div>
                 <h3>{title}</h3>
-                <img src={img} alt='/' />
-                <span className='infos'>
+                <img src={img} alt='/' onClick={this.handleInfo}/>
+                <span className='infos' onClick={this.handleInfo}>
                     <i className='fas fa-plus-circle'></i>
                 </span>
+
+                {
+                    this.state.showInfo && (
+                        <div className="showInfos">
+                            <div className="infosContent">
+                                <div className="head">
+                                    <h2>{title}</h2>
+                                    <div className="sourceCode">
+                                        <a href={link} rel="noopener noreferrer" className='button' target='_blank'>Code source</a>
+                                    </div>
+                                </div>
+
+                                <p className="text">{resume}</p>
+
+                                <div className="button return" onClick={this.handleInfo}>
+                                    Retour
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
             </div>
         );
     }
