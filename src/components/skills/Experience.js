9c9ebf67cb039c1b41cdab035209eb1ef864ab6c
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-09-02 00:50:54 +0200

    detail experiences

--- src/components/skills/Experience.js
+++ src/components/skills/Experience.js
@@ -4,20 +4,31 @@ const Experience = () => {
     return (
         <div className="experience">
             <h3>Expériences</h3>
+            <div className="Python">
+                <h4><i className="fas fa-graduation-cap"></i> Parcours Développeur d'application Python</h4>
+                <h5>2021</h5>
+            </div>
             <div className="Enseignement">
-                <h4>Enseignant de Mathématiques</h4>
-                <h5>Dates-Dates</h5>
-                <p>
-                    Contenu
-                </p>
+                <h4><i className="fas fa-briefcase"></i> Enseignant de Mathématiques</h4>
+                <h5>2016-2020</h5>
             </div>
-            <div className="exp2">
-                <h4>Développeur d'application Python</h4>
-                <h5>Novembre 2020</h5>
-                <p>
-                    Contenu
-                </p>
+            <div className="CAPES">
+                <h4><i className="fas fa-graduation-cap"></i> Obtention du CAPES</h4>
+                <h5>2015</h5>
             </div>
+            <div className="Finance">
+                <h4><i className="fas fa-briefcase"></i> Stage - Risque de Marché</h4>
+                <h5>2015</h5>
+            </div>
+            <div className="Assurance">
+                <h4><i className="fas fa-briefcase"></i> Stage - Inventaire Prévoyance</h4>
+                <h5>2014</h5>
+            </div>
+            <div className="Actuariat">
+                <h4><i className="fas fa-graduation-cap"></i> Master d'actuariat</h4>
+                <h5>2011-2015</h5>
+            </div>
+
         </div>
     );
 };
